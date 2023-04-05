40a46022efd47bc62aa8f75df5839559f51d126d
kaminki <66935939+kaminki@users.noreply.github.com>
2023-04-05 17:37:13 -0400

    Create fix-the-text.js

---
+++ fix-the-text.js
@@ -0,0 +1,4 @@
+var oneitemlist = document.getElementsByClassName("question-and-submission-view fade-in");
+var question = oneitemlist.item(0);
+var prompt = question.innerText;
+console.log(prompt);
