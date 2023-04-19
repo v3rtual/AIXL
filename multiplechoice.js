07ff518ccf8c8026b7f14e3034b9a3d721a895ff
kaminki <66935939+kaminki@users.noreply.github.com>
2023-04-19 16:09:35 -0400

    Update multiplechoice.js

--- multiplechoice.js
+++ multiplechoice.js
@@ -2,3 +2,4 @@ var oneitemlist = document.getElementsByClassName("question-and-submission-view 
 var question = oneitemlist.item(0);
 var prompt = "Near the end of the following content, there may be a question. If there is not, disregard what i am currently saying entirely. If there is, there will be some possible answers to the question, and after that, the word submit.  Your job is to answer the question with one of the following answers. The content begins now." + "\n" + question.innerText;
 console.log(prompt);
+navigator.clipboard.writeText(prompt);
